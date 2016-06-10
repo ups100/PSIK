1bb0b9514e998e0f2d30cdd0794862f486919e81
Krzysztof Opasiak <kopasiak90@gmail.com>
2016-06-10 16:00:48 +0200

    Add script to deploy code to vms
    Signed-off-by: Krzysztof Opasiak <kopasiak90@gmail.com>

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+
+# Top level of repo should contain symlinks to sshfs mounted roots of both vms
+GIT_TOP=`git rev-parse --show-toplevel`
+MININET_VM="./mininet-vm"
+POX_VM="./pox-vm"
+
+cd $GIT_TOP
+
+cp "./mininet/topo.py" "$MININET_VM/home/mininet/"
+
+find pox -name *.py | xargs -L1 -I'{}' cp '{}' "$POX_VM/home/mininet/pox/ext/"
